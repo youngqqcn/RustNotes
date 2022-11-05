6714877a6c808520234b4d5b2c9615741de3a4e7
yqq <youngqqcn@gmail.com>
2022-11-05 22:49:03 +0800

    使用 let 绑定变量

---
+++ rust-by-example/4-变量绑定/1-变量绑定.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let an_integer = 1u32;
+    let a_boolean = true;
+    let unit = (); // 单元类型  (unit type)
+
+    // 将 `an_integer` 复制到 `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {:?}", copied_integer);
+    println!("A boolean: {:?}", a_boolean);
+    println!("Meet the unit value: {:?}", unit);
+
+    // 编译器会对未使用的变量绑定产生警告；可以给变量名加上下划线前缀来消除警告。
+    let _unused_variable = 3u32;
+
+    let _noisy_unused_variable = 2u32;
+    // 改正 ^ 在变量名前加上下划线以消除警告
+}
